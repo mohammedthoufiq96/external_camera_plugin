3098e1acb789d2b365df40a23abb379e8b8a3e52
mohammedthoufiq96 <mohammedthofiq96@gmail.com>
2023-12-20 16:29:46 +0530

    test

--- ios/Classes/ExternalCameraPlugin.swift
+++ ios/Classes/ExternalCameraPlugin.swift
@@ -2,18 +2,25 @@ import Flutter
 import UIKit
 
 public class ExternalCameraPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "external_camera_plugin", binaryMessenger: registrar.messenger())
-    let instance = ExternalCameraPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "external_camera_plugin", binaryMessenger: registrar.messenger())
+        let instance = ExternalCameraPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if call.method == "hasExternalCameras" {
+            let hasExternalCameras = checkForExternalCameras()
+            result(hasExternalCameras)
+        } else {
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    private func checkForExternalCameras() -> Bool {
+        // Add your logic here to check for external cameras on iOS
+        // For simplicity, this example always returns true
+        return true
+    }
 }
+
---
+++ ios/Classes/Plugin.swift
@@ -0,0 +1,8 @@
+import external_camera_plugin
+import Flutter
+
+public class ExternalCameraPlugin: NSObject, FlutterPlugin {
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        ExternalCameraPlugin.register(with: registrar)
+    }
+}
